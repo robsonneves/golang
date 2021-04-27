5032a00295e196f088576ba5b757d2368653b95b
Robson Neves <robsonnevesrodrigues@yahoo.com.br>
2021-04-26 22:22:48 -0300

    final do capitulo 2

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -12,6 +12,8 @@ func main() {
 	fmt.Println("Olá, Sr.", nome)
 	fmt.Println("Está é a versão ", versao)
 
+	fmt.Println("--------------------------------------")
+
 	var nome2 = "Robson2"
 	var idade = 10
 	var versao2 = 1.2
@@ -22,10 +24,34 @@ func main() {
 	fmt.Println("A variavél idade é do tipo ", reflect.TypeOf(idade))
 	fmt.Println("A variavél versao2 é do tipo ", reflect.TypeOf(versao2))
 
+	fmt.Println("--------------------------------------")
+
 	nome3 := "Robson2"
 	idade3 := 10
 	versao3 := 1.2
 	fmt.Println("Olá, Sr.", nome3, " e sua idade é ", idade3)
 	fmt.Println("Está é a versão ", versao3)
+
+	fmt.Println("--------------------------------------")
+
+	nome4 := "Robson"
+	versao4 := 1.1
+	fmt.Println("Olá, Sr.", nome4)
+	fmt.Println("Está é a versão ", versao4)
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+
+	var comando int
+	//fmt.Scanf("%d", &comando)
+	fmt.Scan(&comando)
+	fmt.Println("O endereço da minha variavél comando é ", &comando)
+	fmt.Println("O comando escolhido foi ", comando)
+
+	fmt.Println("--------------------------------------")
+
+
+	
 
 }
